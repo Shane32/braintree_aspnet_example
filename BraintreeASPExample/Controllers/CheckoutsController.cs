4b732e187662a4e03bb49c736992d972ac83e014
Shane Krueger <Shane@acdmail.com>
2020-04-14 17:19:09 -0400

    Update to MVC 5 and .Net Framework 4.7.2

--- BraintreeASPExample/Controllers/CheckoutsController.cs
+++ BraintreeASPExample/Controllers/CheckoutsController.cs
@@ -32,7 +32,7 @@ namespace BraintreeASPExample.Controllers
         public ActionResult Create()
         {
             var gateway = config.GetGateway();
-            Decimal amount;
+            decimal amount;
 
             try
             {
