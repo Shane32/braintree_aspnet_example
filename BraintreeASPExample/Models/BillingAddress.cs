dc79b36cf6a8cf7602839231fcd7a06e6e032745
Christian <christian@zbox.com>
2020-04-21 13:39:38 -0400

    made googlepay pass to second page for retry

---
+++ BraintreeASPExample/Models/BillingAddress.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace BraintreeASPExample.Models
+{
+    public partial class BillingAddress
+    {
+        [JsonProperty("phoneNumber")]
+        public string PhoneNumber { get; set; }
+
+        [JsonProperty("address3")]
+        public string Address3 { get; set; }
+
+        [JsonProperty("sortingCode")]
+        public string SortingCode { get; set; }
+
+        [JsonProperty("address2")]
+        public string Address2 { get; set; }
+
+        [JsonProperty("countryCode")]
+        public string CountryCode { get; set; }
+
+        [JsonProperty("address1")]
+        public string Address1 { get; set; }
+
+        [JsonProperty("postalCode")]
+        public string PostalCode { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("locality")]
+        public string City { get; set; }
+
+        [JsonProperty("administrativeArea")]
+        public string State { get; set; }
+    }
+}
